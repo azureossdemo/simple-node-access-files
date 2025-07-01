8219301e3186e106eb06cf4f6d8b579e75ac09c4
azureossdemo <ossdemo454242@outlook.com>
2025-07-01 08:57:26 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,22 +5,34 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const filesDir = path.join(__dirname, 'files');
+
 // Home route
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome to the Home Page</h1><p>Go to <a href="/files/test.txt">/files/test.txt</a></p>');
+  res.send('<h1>Welcome to the Home Page</h1><p>Go to <a href="/files">/files</a> to browse files.</p>');
 });
 
-// Route to serve the test.txt file
-app.get('/files/test.txt', (req, res) => {
-  const filePath = path.join(__dirname, 'files', 'test.txt');
+// File browser: list all files in /files
+app.get('/files', (req, res) => {
+  fs.readdir(filesDir, (err, files) => {
+    if (err) {
+      return res.status(500).send('Unable to read files directory.');
+    }
 
-  // Check if file exists and send it
+    const list = files.map(file => `<li><a href="/files/${file}">${file}</a></li>`).join('');
+    res.send(`<h2>Files</h2><ul>${list}</ul>`);
+  });
+});
+
+// Serve any file from /files folder
+app.get('/files/:filename', (req, res) => {
+  const filePath = path.join(filesDir, req.params.filename);
+
   fs.access(filePath, fs.constants.F_OK, (err) => {
     if (err) {
-      res.status(404).send('File not found');
-    } else {
-      res.sendFile(filePath);
+      return res.status(404).send('File not found.');
     }
+    res.sendFile(filePath);
   });
 });
 
